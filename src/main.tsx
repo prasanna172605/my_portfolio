70ad64945ae21fa9c8bd93af7eaa25c5902d4226
Prasanna Iyappan <named.as.prasanna@gmail.com>
2026-08-30 16:49:38 +0530

    fix(theme): wrap application with ThemeProvider in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { HelmetProvider } from 'react-helmet-async'
+import { ThemeProvider } from './components/ThemeProvider'
 import './index.css'
 import App from './App.tsx'
 
@@ -9,7 +10,9 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <HelmetProvider>
       <BrowserRouter>
-        <App />
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
       </BrowserRouter>
     </HelmetProvider>
   </StrictMode>,
